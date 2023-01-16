031793d8286dcf963ce5271e07a988ebfe775ad3
jon-vdb <jonvdberg8@gmail.com>
2023-01-16 21:40:43 +0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "source.unsplash.com",
+      "landmark-real-eastate.s3.ap-southeast-1.amazonaws.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react";
+import { ApolloProvider } from "@apollo/client";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Footer } from "../components/Footer";
+import { Nav } from "../components/Nav";
+import { useClient } from "../lib/client";
+import { Session } from "next-auth";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{
+  session: Session;
+}>) {
+  const client = useClient();
+  return (
+    <SessionProvider session={session}>
+      <ApolloProvider client={client}>
+        <Nav />
+        <div className="pt-[6.1rem]">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </ApolloProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default App;
